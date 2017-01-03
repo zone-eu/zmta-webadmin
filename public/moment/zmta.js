42c5815d8570f788821aba70accb2c00b30e40cc
Andris Reinman <andris@kreata.ee>
2017-01-03 09:55:13 +0200

    u

--- public/moment/zmta.js
+++ public/moment/zmta.js
@@ -5,6 +5,6 @@ var date;
 for (var i = 0, len = elms.length; i < len; i++) {
     elm = elms[i];
     if (elm.title && elm.title.length === 24) {
-        elm.textContent = moment(elm.title).format('YYYY/MM/DD hh:mm:ss');
+        elm.textContent = moment(elm.title).format('YYYY-MM-DD hh:mm:ss');
     }
 }
