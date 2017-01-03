cfeea78a553cad3c7af59d98e80a7b46abbea688
Andris Reinman <andris@kreata.ee>
2017-01-03 10:17:57 +0200

    u

--- public/moment/zmta.js
+++ public/moment/zmta.js
@@ -1,10 +1,28 @@
-var elms = document.querySelectorAll('.datestring');
-var elm;
-var date;
+function updateDatestrings() {
+    var elms = document.querySelectorAll('.datestring');
+    var elm;
 
-for (var i = 0, len = elms.length; i < len; i++) {
-    elm = elms[i];
-    if (elm.title && elm.title.length === 24) {
-        elm.textContent = moment(elm.title).format('YYYY-MM-DD hh:mm:ss');
+    for (var i = 0, len = elms.length; i < len; i++) {
+        elm = elms[i];
+        if (elm.title && elm.title.length === 24) {
+            elm.textContent = moment(elm.title).format('YYYY-MM-DD hh:mm:ss');
+        }
     }
 }
+
+function updateRelativeDatestrings() {
+    var elms = document.querySelectorAll('.datestring-relative');
+    var elm;
+
+    for (var i = 0, len = elms.length; i < len; i++) {
+        elm = elms[i];
+        if (elm.title && elm.title.length === 24) {
+            elm.textContent = moment(elm.title).fromNow();
+        }
+    }
+}
+
+updateDatestrings();
+updateRelativeDatestrings();
+
+setInterval(updateRelativeDatestrings, 10 * 1000);
